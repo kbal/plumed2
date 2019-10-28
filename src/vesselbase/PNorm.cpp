073010d6bbc5e5fd96eec07db67719062cf1b487
Kristof Bal <kristof.bal@gmail.com>
2019-10-28 16:22:50 +0100

    Make PNorm vessel always available.
    Putting a vessel in an optional module gives issues when this module isn't enabled. It is only active for a few multicolvars for now, based on my own applications.

--- src/vesselbase/PNorm.cpp
+++ src/vesselbase/PNorm.cpp
@@ -19,9 +19,9 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
-#include "vesselbase/VesselRegister.h"
-#include "vesselbase/FunctionVessel.h"
-#include "vesselbase/ActionWithVessel.h"
+#include "VesselRegister.h"
+#include "FunctionVessel.h"
+#include "ActionWithVessel.h"
 
 namespace PLMD {
 namespace vesselbase {
