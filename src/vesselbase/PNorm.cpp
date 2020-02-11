4dfa36f87d87ffd0b62b06ecdab04202cf3ea7d2
Kristof Bal <kristof.bal@gmail.com>
2020-02-11 17:04:05 +0100

    Fix license.

--- src/vesselbase/PNorm.cpp
+++ src/vesselbase/PNorm.cpp
@@ -1,24 +1,24 @@
 /* +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
- *    Copyright (c) 2013-2017 The plumed team
- *       (see the PEOPLE file at the root of the distribution for a list of names)
- *
- *          See http://www.plumed.org for more information.
- *
- *             This file is part of plumed, version 2.
- *
- *                plumed is free software: you can redistribute it and/or modify
- *                   it under the terms of the GNU Lesser General Public License as published by
- *                      the Free Software Foundation, either version 3 of the License, or
- *                         (at your option) any later version.
- *
- *                            plumed is distributed in the hope that it will be useful,
- *                               but WITHOUT ANY WARRANTY; without even the implied warranty of
- *                                  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *                                     GNU Lesser General Public License for more details.
- *
- *                                        You should have received a copy of the GNU Lesser General Public License
- *                                           along with plumed.  If not, see <http://www.gnu.org/licenses/>.
- *                                           +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+   Copyright (c) 2013-2017 The plumed team
+   (see the PEOPLE file at the root of the distribution for a list of names)
+
+   See http://www.plumed.org for more information.
+
+   This file is part of plumed, version 2.
+
+   plumed is free software: you can redistribute it and/or modify
+   it under the terms of the GNU Lesser General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   plumed is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with plumed.  If not, see <http://www.gnu.org/licenses/>.
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
 #include "VesselRegister.h"
 #include "FunctionVessel.h"
 #include "ActionWithVessel.h"
@@ -30,6 +30,7 @@ class PNorm : public FunctionVessel {
 private:
   double power;
   double shift;
+  double scale;
 public:
   static void registerKeywords( Keywords& keys );
   static void reserveKeyword( Keywords& keys );
@@ -45,6 +46,7 @@ void PNorm::registerKeywords( Keywords& keys ) {
   FunctionVessel::registerKeywords( keys );
   keys.add("compulsory","P","the value of P");
   keys.add("compulsory","SHIFT","the value by which values are shifted before taking the norm");
+  keys.add("compulsory","SCALE","the value by which values (after shifting) are multiplied");
 }
 
 void PNorm::reserveKeyword( Keywords& keys ) {
@@ -59,6 +61,7 @@ PNorm::PNorm( const VesselOptions& da ) :
   if( getAction()->isPeriodic() ) error("PNORM is not a meaningful option for periodic variables");
   parse("P",power);
   parse("SHIFT",shift);
+  parse("SCALE",scale);
   if( diffweight ) error("can't calculate PNORM if weight is differentiable");
 }
 
@@ -69,7 +72,8 @@ std::string PNorm::value_descriptor() {
 
 double PNorm::calcTransform( const double& val, double& dv ) const {
   double temp = val-shift;
-  double f = pow(temp, power); dv=power*f/temp; return f;
+  temp *= scale;
+  double f = pow(temp, power); dv=power*f*scale/temp; return f;
 }
 
 double PNorm::finalTransform( const double& val, double& dv ) {
