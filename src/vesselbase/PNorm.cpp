701602b6f9ab79fda4649ce534adfe2d2e1de3af
Kristof Bal <kristof.bal@gmail.com>
2020-02-11 17:08:25 +0100

    Style.

--- src/vesselbase/PNorm.cpp
+++ src/vesselbase/PNorm.cpp
@@ -19,6 +19,7 @@
    You should have received a copy of the GNU Lesser General Public License
    along with plumed.  If not, see <http://www.gnu.org/licenses/>.
 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ */
+
 #include "VesselRegister.h"
 #include "FunctionVessel.h"
 #include "ActionWithVessel.h"
